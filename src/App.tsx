85bd775dd36601320c22f40a9e8bf4992fd77cad
JoaoGF03 <joao.g.f@icloud.com>
2021-08-25 16:18:33 -0300

    Styling Modal Overlay

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import { useState } from 'react';
 import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from './styles/global';
+
+Modal.setAppElement('#root')
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
@@ -18,14 +21,14 @@ export function App() {
   return (
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      
       <Dashboard />
-      <Modal
+
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-        contentLabel="Example Modal"
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      />
+
       <GlobalStyle />
     </>
   );
